dcea53a5057fa4b95c869f7f5def480073e19681
Udbhaw Anand <udbhawinsta@gmail.com>
2025-08-05 01:49:53 +0530

    automated tests for FormateCurrency

---
+++ Html,Css&Js/JavaSript/JavaScript_Amazon/tests/moneyTest.js
@@ -0,0 +1,36 @@
+import {formatCurrency} from '../scripts/utils/money.js';
+
+console.log('test suite;formatCurrency');
+
+console.log('converts cents into dollors')
+;
+
+
+
+if(formatCurrency(2095)==='20.95'){
+console.log('passed');
+}else{
+  console.log('fail');
+}
+
+
+
+
+console.log('works with zero')
+;
+
+if(formatCurrency(0)==='0.00'){
+  console.log('passed');
+}else{
+  console.log('fail');
+}
+
+
+console.log('rounds up to the nearest cents')
+
+
+if(formatCurrency(2000.4)==='20.00'){
+console.log('passed');
+}else{
+  console.log('fail');
+}
